bf4ee38d8983cba2f1dd585fa157a0e7cf0f4eb6
seekweeteck <seekt@tarc.edu.my>
2024-11-21 13:50:50 +0800

    Initial

---
+++ lib/data.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+class Document {
+  final Map<String, Object?> _json;
+
+  Document() : _json = jsonDecode(documentJson);
+
+  (String, {DateTime modified}) get metadata {
+    // Add from here...
+    if (_json // Modify from here...
+        case {
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
+    } // to here.
+  }
+
+  List<Block> getBlocks() {                                  // Add from here...
+    if (_json case {'blocks': List blocksJson}) {
+      return [for (final blockJson in blocksJson) Block.fromJson(blockJson)];
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+class Block {
+  final String type;
+  final String text;
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': final type, 'text': final text}) {
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+const documentJson = '''
+{
+  "metadata": {
+    "title": "My Document",
+    "modified": "2023-05-10"
+  },
+  "blocks": [
+    {
+      "type": "h1",
+      "text": "Chapter 1"
+    },
+    {
+      "type": "p",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      "type": "checkbox",
+      "checked": false,
+      "text": "Learn Dart 3"
+    }
+  ]
+}
+''';
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import 'data.dart';
+
+void main() {
+  runApp(const DocumentApp());
+}
+
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+
+  const DocumentScreen({
+    required this.document,
+    super.key,
+  });            // Add this line.
+
+  @override
+  Widget build(BuildContext context) {
+    final (title, :modified) = document.metadata;            // Modify
+    final blocks = document.getBlocks();
+
+    return Scaffold(
+      appBar: AppBar(
+         title: Text('Last modified: $modified'),                    // Modify this line,
+      ),
+      body: Column(
+        children: [
+          Text('Last modified: $modified'),                        // Modify from here
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ),                                                       // to here.
+        ],
+      ),
+    );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      case _:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
+      ),
+    );
+  }
+}
